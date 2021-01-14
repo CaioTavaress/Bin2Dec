cbb21cf2919ee6a928b94e89639d9db78346213f
Caio Francisco Tavares de Lima <56778786+CaioTavaress@users.noreply.github.com>
2021-01-14 20:15:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
    const BIN = document.getElementById('binary-value')
    const DEC = document.getElementById('decimal-value') 
    document.getElementById('convert-bin').addEventListener('click', () => {
-      if(BIN.value != '0' || BIN.value != '1'){
+      if(BIN.value != 0 || BIN.value != 1){
       alerta('Insira um valor binário válido')
       } else {
    DEC.value = parseInt(BIN.value, 2);
