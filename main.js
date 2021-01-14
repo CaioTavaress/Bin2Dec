eedc4ddf02b9dc1cf7a96a046cb3933db7179f39
Caio Francisco Tavares de Lima <56778786+CaioTavaress@users.noreply.github.com>
2021-01-14 20:11:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,5 @@
    const BIN = document.getElementById('binary-value')
    const DEC = document.getElementById('decimal-value') 
    document.getElementById('convert-bin').addEventListener('click', () => {
-      let convertToDec = parseInt(BIN.value, 2);
-      DEC.value = convertToDec
+      DEC.value = parseInt(BIN.value, 2);
    })
